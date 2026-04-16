ea1ad02a65e0f8291681d91f7744d7a55ab8a6be
Mo Moin <momoin737@gmail.com>
2026-04-16 15:40:06 +0530

    optimize for deployment

--- src/models.py
+++ src/models.py
@@ -4,8 +4,8 @@ from sklearn.linear_model import LogisticRegression
 
 def get_models():
     return {
-        'Random Forest': RandomForestClassifier(n_estimators=200, random_state=42),
-        'Gradient Boosting': GradientBoostingClassifier(n_estimators=150, random_state=42),
+        'Random Forest': RandomForestClassifier(n_estimators=100, random_state=42),
+        'Gradient Boosting': GradientBoostingClassifier(n_estimators= 80, random_state=42),
         'SVM (RBF)': SVC(kernel='rbf', C=10, probability=True, random_state=42),
         'Logistic Regression': LogisticRegression(max_iter=1000, random_state=42)
     }
